ac917f320d286588115d9327f5f12a8fc557c4ad
Skunk-XCP <skunk-xcp@hotmail.fr>
2024-10-16 10:56:27 +0200

    Add style to TaskBar

--- src/components/TaskBar.vue
+++ src/components/TaskBar.vue
@@ -2,11 +2,31 @@
   <div
     class="flex justify-evenly border-0 border-t-2 border-t-zinc-200 items-center p-2 m-2"
   >
-    <Icon icon="ic:round-home" class="h-6 w-6" />
-    <Icon icon="mdi:calendar-outline" class="h-6 w-6" />
-    <Icon icon="streamline:add-circle-solid" class="h-8 w-8 text-blue-400" />
-    <Icon icon="mdi:chart-line" class="h-6 w-6" />
-    <Icon icon="ri:more-fill" class="h-6 w-6" />
+    <button
+      class="p-2 rounded-lg transition duration-200 hover:bg-gray-200 hover:ring-2 hover:ring-gray-200"
+    >
+      <Icon icon="ic:round-home" class="h-6 w-6" />
+    </button>
+    <button
+      class="p-2 rounded-lg transition duration-200 hover:bg-gray-200 hover:ring-2 hover:ring-gray-200"
+    >
+      <Icon icon="mdi:calendar-outline" class="h-6 w-6" />
+    </button>
+    <button
+      class="p-2 rounded-lg transition duration-200 hover:bg-gray-200 hover:ring-2 hover:ring-gray-200"
+    >
+      <Icon icon="streamline:add-circle-solid" class="h-8 w-8 text-blue-400" />
+    </button>
+    <button
+      class="p-2 rounded-lg transition duration-200 hover:bg-gray-200 hover:ring-2 hover:ring-gray-200"
+    >
+      <Icon icon="mdi:chart-line" class="h-6 w-6" />
+    </button>
+    <button
+      class="p-2 rounded-lg transition duration-200 hover:bg-gray-200 hover:ring-2 hover:ring-gray-200"
+    >
+      <Icon icon="ri:more-fill" class="h-6 w-6" />
+    </button>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,12 @@ const expenses = 2000
 <template>
   <main>
     <FinanceCard :income="income" :expenses="expenses" />
+    <div
+      class="bg-zinc-200 h-96 mt-4 flex items-center justify-center"
+      style="height: 70vh"
+    >
+      <p class="text-zinc-400 text-xl font-bold text-center">No data</p>
+    </div>
     <TaskBar />
   </main>
 </template>
